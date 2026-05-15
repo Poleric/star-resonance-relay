93ad022bbf38a0250c98843ad5641b03cc230c66
Poleric <laffey@dizzynight.moe>
2026-05-15 11:15:08 +0800

    feat: use back custom tcp reassembler

--- src/star_resonance_relay/sniffer/sniffer.py
+++ src/star_resonance_relay/sniffer/sniffer.py
@@ -2,19 +2,20 @@ import logging
 from collections import defaultdict
 from typing import Callable
 
+from google.protobuf.message import Message
+from scapy.config import conf
+from scapy.layers.inet import TCP, IP
+from scapy.packet import Packet, Raw
 from scapy.sendrecv import sniff
 from scapy.sessions import TCPSession
-from google.protobuf.message import Message
-from scapy.layers.inet import TCP
-from scapy.packet import Packet, Raw
 
-from star_resonance_relay.packet.processor import BPSRPacketProcessor
 from star_resonance_relay.packet.msg import CallMsg, NotifyMsg, ReturnMsg
-from star_resonance_relay.sniffer import scapy as sniffer_scapy
-from star_resonance_relay.sniffer.scapy import BPSRPacket
+from star_resonance_relay.packet.processor import BPSRPacketProcessor
 from star_resonance_relay.sniffer.connection import Connection
+from star_resonance_relay.utils import TCPReassembler
 
 logger = logging.getLogger(__name__)
+conf.layers.filter([TCP, IP])
 
 
 class BPSRSniffer:
@@ -22,6 +23,8 @@ class BPSRSniffer:
 
     def __init__[T: Message, K: Message](self):
         self._processor = BPSRPacketProcessor()
+        self._reassemblers: dict[Connection, TCPReassembler] = defaultdict(TCPReassembler)
+        self._known_servers: set[Connection] = set()
         self._handlers: dict[type[T], list[Callable[[T], None]]] = defaultdict(list)
 
         self._service_types: dict[tuple[int, int], type[T]] = {}
@@ -45,58 +48,57 @@ class BPSRSniffer:
             return
 
         connection = Connection.from_packet(packet)
-
-        if BPSRPacket not in packet:
-            if connection not in sniffer_scapy.KNOWN_SERVERS:
-                payload = bytes(packet[Raw])
-
-                # Discover/lock server flow
-                if self._is_server(payload):
-                    logger.info(f"Adding to flow "
-                                f"{connection.source.ip}:{connection.source.port} <-> "
-                                f"{connection.destination.ip}:{connection.destination.port}")
-                    sniffer_scapy.KNOWN_SERVERS.add(connection)
+        if connection not in self._known_servers:
+            payload = bytes(packet[Raw])
+            # Discover/lock server flow
+            if self._is_server(payload):
+                logger.info(f"Adding to flow "
+                            f"{connection.source.ip}:{connection.source.port} <-> "
+                            f"{connection.destination.ip}:{connection.destination.port}")
+                self._known_servers.add(connection)
 
             return
 
         try:
-            logger.info(f"{connection.source.ip} <-> {connection.destination.ip}: {packet}")
-            bpsr_packet = packet[BPSRPacket]
+            payload = bytes(packet[Raw])
 
-            for msg in self._processor.process_bytes(bytes(bpsr_packet)):
-                match msg:
-                    case CallMsg() | NotifyMsg():
-                        msg_type = self._service_types.get((msg.service_uuid, msg.method_id))
-                        if msg_type is None:
+            self._reassemblers[connection].push(packet[TCP].seq, payload)
+            logger.info(self._reassemblers[connection].cache)
+            for frame in self._reassemblers[connection].pop_frames():
+                for msg in self._processor.process_bytes(frame):
+                    match msg:
+                        case CallMsg() | NotifyMsg():
+                            msg_type = self._service_types.get((msg.service_uuid, msg.method_id))
+                            if msg_type is None:
+                                continue
+
+                            if isinstance(msg, CallMsg):
+                                self._calls[msg.call_id] = msg_type
+
+                            handlers = self._handlers.get(msg_type, [])
+                            for handler in handlers:
+                                handler(msg_type.FromString(msg.data))
+
+                        case ReturnMsg():
+                            msg_type = self._calls.get(msg.call_id)
+                            if msg_type is None:
+                                continue
+
+                            del self._calls[msg.call_id]
+
+                            return_type = self._return_types.get(msg_type)
+                            if return_type is None:
+                                continue
+
+                            handlers = self._handlers.get(return_type, [])
+                            for handler in handlers:
+                                handler(return_type.FromString(msg.data))
+
+                        case _:
                             continue
-
-                        if isinstance(msg, CallMsg):
-                            self._calls[msg.call_id] = msg_type
-
-                        handlers = self._handlers.get(msg_type, [])
-                        for handler in handlers:
-                            handler(msg_type.FromString(msg.data))
-
-                    case ReturnMsg():
-                        msg_type = self._calls.get(msg.call_id)
-                        if msg_type is None:
-                            continue
-
-                        del self._calls[msg.call_id]
-
-                        return_type = self._return_types.get(msg_type)
-                        if return_type is None:
-                            continue
-
-                        handlers = self._handlers.get(return_type, [])
-                        for handler in handlers:
-                            handler(return_type.FromString(msg.data))
-
-                    case _:
-                        continue
 
         except Exception:
             logger.exception(packet)
 
     def sniff(self):
-        sniff(filter="tcp", prn=self.handle_packet, store=False, session=TCPSession)
+        sniff(filter="tcp and ip", prn=self.handle_packet, store=False, session=TCPSession)
